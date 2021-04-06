c29da6ba0200e97466b923257d516f3436b5b95d
Marcel Vidmar <marci.vidmar@gmail.com>
2021-04-06 13:38:12 +0200

    initial Commit Datenbank-Verbindung hergestellt

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <Title>Ein neuer Versuch</Title>
+</head>
+<body>
+    <?php
+		$db_link = mysqli_connect (
+						 "db4free.net:3306", 
+						 "virustestshouser", 
+						 "wfjkg4P2Na9W5df", 
+						 "virustestshopdb"
+						);
+		$sql = "SELECT * FROM adressen";
+
+		$db_erg = mysqli_query( $db_link, $sql );
+		if ( ! $db_erg )
+		{
+		echo "FEHLER!";
+		  die('Ungültige Abfrage: ' . mysqli_error());
+		} else{
+		echo "erfolgreich!";
+		}
+					
+    ?>
+</body>
+</html>
