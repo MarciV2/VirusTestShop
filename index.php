5288eefea6ce2823ba23026fea4e9feaa44bf72a
Marcel Vidmar <marci.vidmar@gmail.com>
2021-04-20 12:57:00 +0200

    cleaned up

--- index.php
+++ index.php
@@ -1,27 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <Title>Ein neuer Versuch</Title>
+    <Title>VirusTestShop</Title>
+	<link rel="shortcut icon" href="http://virustestshop.azurewebsites.net/logo.png">
 </head>
 <body>
-    <?php
-		$db_link = mysqli_connect (
-						 "db4free.net:3306", 
-						 "virustestshouser", 
-						 "wfjkg4P2Na9W5df", 
-						 "virustestshopdb"
-						);
-		$sql = "SELECT * FROM DUAL";
+	<!-- <?php
+	$db_link = mysqli_connect (
+	"db4free.net:3306",
+	"virustestshouser",
+	"wfjkg4P2Na9W5df", //rwU64HdFv!hMX
+	"virustestshopdb"
+	);
+	$sql = "SELECT * FROM DUAL";
 
-		$db_erg = mysqli_query( $db_link, $sql );
-		if ( ! $db_erg )
-		{
-		echo "FEHLER!";
-		  die('Ungültige Abfrage: ' . mysqli_error());
-		} else{
-		echo "erfolgreich!";
-		}
-					
-    ?>
+	$db_erg = mysqli_query( $db_link, $sql );
+	if ( ! $db_erg )
+	{
+	echo "FEHLER!";
+	die('Ungültige Abfrage: ' . mysqli_error());
+	} else{
+	echo "erfolgreich!";
+	}
+
+	?> -->
 </body>
 </html>
