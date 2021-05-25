7e17b5f61700ed34f2b1ef664d3302637f3a8d8f
Marcel Vidmar <marci.vidmar@gmail.com>
2021-05-25 12:53:18 +0200

    produktangebot - warenkorb, weiterlesen&detail-seite produktseite: dynamischer datensatz, nur titel

--- content/produktangebot.php
+++ content/produktangebot.php
@@ -105,32 +105,27 @@
         </div>
         <!-- Background image -->
         <script>
+
+        //GET-Parameter an URL anhängen
         function URL_add_parameter(url, param, value){
-                                        var hash       = {};
-                                        var parser     = document.createElement('a');
+            var hash       = {};
+            var parser     = document.createElement('a');
+            parser.href    = url;
+            var parameters = parser.search.split(/\?|&/);
 
-                                        parser.href    = url;
-
-                                        var parameters = parser.search.split(/\?|&/);
-
-                                        for(var i=0; i < parameters.length; i++) {
-                                            if(!parameters[i])
-                                                continue;
-
-                                            var ary      = parameters[i].split('=');
-                                            hash[ary[0]] = ary[1];
-                                        }
-
-                                        hash[param] = value;
-
-                                        var list = [];  
-                                        Object.keys(hash).forEach(function (key) {
-                                            list.push(key + '=' + hash[key]);
-                                        });
-
-                                        parser.search = '?' + list.join('&');
-                                        return parser.href;
-                                    }
+            for(var i=0; i < parameters.length; i++) {
+                if(!parameters[i]) continue;
+            var ary      = parameters[i].split('=');
+                hash[ary[0]] = ary[1];
+            }
+            hash[param] = value;
+            var list = [];  
+            Object.keys(hash).forEach(function (key) {
+                list.push(key + '=' + hash[key]);
+            });
+            parser.search = '?' + list.join('&');
+                return parser.href;
+            }
                                    
         </script>
         <div style="padding: 25px; background-color: #f9f9f9">
@@ -247,7 +242,7 @@
                     var product_description="";
                     if(packung[3].length<=64)product_description=packung[3];
                     else {
-                        product_description=packung[3].substring(0,54)+"... <a href=''>weiter lesen</a>";
+                        product_description=packung[3].substring(0,54)+"... <a href='./produkt.html?product_id="+product_id+"'>weiter lesen</a>";
                     }
                     var product_preis=packung[5]+"€";
 
@@ -263,10 +258,10 @@
                     itemCard = itemCard + "</p>";
                     itemCard = itemCard + "<div class='row no-gutters'>";
                     itemCard = itemCard + "<div class='col-md-7'>";
-                    itemCard = itemCard + "<a class='btn btn-primary btn-rounded buttonTocartMarginBottom' onclick=\"addProductToCart(\'" + "product_name" + "\', " + "product_id" + ")\" style='display: block; background-color: #1E90FF'>In den Warenkorb</a>";
+                    itemCard = itemCard + "<a class='btn btn-primary btn-rounded buttonTocartMarginBottom' onclick=\"addProductToCart(\'" + product_name + "\', " + product_id + ")\" style='display: block; background-color: #1E90FF'>In den Warenkorb</a>";
                     itemCard = itemCard + "</div>";
                     itemCard = itemCard + "<div class='col-md-5'>";
-                    itemCard = itemCard + "<a href='./produkt.html' class='btn btn-primary btn-rounded' style='display: block; background-color: #1E90FF'>Details</a>";
+                    itemCard = itemCard + "<a href='./produkt.html?product_id="+product_id+"' class='btn btn-primary btn-rounded' style='display: block; background-color: #1E90FF'>Details</a>";
                     itemCard = itemCard + "</div>";
                     itemCard = itemCard + "</div>";
                     itemCard = itemCard + "</div>";
