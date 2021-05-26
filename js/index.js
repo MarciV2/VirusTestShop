871a804e63b621c3d936291d3713b0c557d0118c
vivebetterthanrift <varusvarus1@gmail.com>
2021-05-26 21:04:11 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -20,3 +20,37 @@ function clickAGBs()
 {
 	window.location = "/content/agb.html";
 }
+function pwprüfen()
+{
+	
+	var pw1 = document.getElementById("passwort").value;
+	var pw2 = document.getElementById("wdhlg-passwort").value;
+	var result = pw1.localeCompare(pw2);
+	
+	 if (result)
+        {
+			
+            alert("Passwörter müssen gleich sein!");
+			pw1 = document.getElementById("passwort").value=null;
+			pw2 = document.getElementById("wdhlg-passwort").value=null;
+            document.getElementById("passwort").focus();
+            return false;
+        }
+}
+function emailchecken()
+{
+	
+	var email1 = document.getElementById("email").value;
+	var email2 = document.getElementById("wdhlg-email").value;
+	var result = email1.localeCompare(email2);
+	
+	 if (result)
+        {
+			
+            alert("Emails müssen gleich sein!");
+			email1 = document.getElementById("email").value=null;
+			email2 = document.getElementById("wdhlg-email").value=null;
+            document.getElementById("email").focus();
+            return false;
+        }
+}
