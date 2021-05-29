1fa3670643c9d7c34966d73bab86954dae508fe9
vivebetterthanrift <varusvarus1@gmail.com>
2021-05-29 14:19:07 +0200

    Entfernen button unten für Seiten

--- content/produktangebot.php
+++ content/produktangebot.php
@@ -296,21 +296,7 @@
 
             <br />
 
-            <div class="card" style="margin: 10px; padding-top: 15px">
-                <nav aria-label="...">
-                    <ul class="pagination pagination-circle justify-content-center">
-                        <li class="page-item">
-                            <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Vorherige Seite</a>
-                        </li>
-                        <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                            <a class="page-link" href="#">Nächste Seite</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+            
         </div>
 
 
