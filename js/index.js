05fdae2443972207b2fb501e1c61e7280f886a20
vivebetterthanrift <varusvarus1@gmail.com>
2021-05-31 13:57:35 +0200

    Kommentare

--- js/index.js
+++ js/index.js
@@ -10,14 +10,15 @@
 //        myNav.classList.remove("windowedPageNavbar");
 //    }
 //};
-
+//Zum überprüfen
 function checkLogin(login) {
     console.log("Test checkLogin" + login);
 }
-
+//Weiterleitung auf AGB von dem Registrierungsform
 function clickAGBs() {
     window.location = "/content/agb.html";
 }
+//Funktion prüft ob passwort bei Registrierung gleich ist wenn nicht werden die Felder gereinigt und ein alert "Passwörter müssen gleich sein!" wird angezeigt
 function pwpruefen() {
     var pw1 = document.getElementById("passwort").value;
     var pw2 = document.getElementById("wdhlg-passwort").value;
@@ -31,6 +32,7 @@ function pwpruefen() {
         return false;
     }
 }
+//Funktion prüft ob mail bei Registrierung gleich ist wenn nicht werden die Felder gereinigt und ein alert "Emails müssen gleich sein!" wird angezeigt
 function emailchecken() {
     var email1 = document.getElementById("email").value;
     var email2 = document.getElementById("wdhlg-email").value;
@@ -45,23 +47,9 @@ function emailchecken() {
         return false;
     }
 }
-
+//Logout script löscht user session somit muss sich neu angemeldet werden
 function logout() {
     document.cookie = "PHPSESSID=; path=/";
     location.reload();
 }
 
-function regSuccess() {
-    new Notify({
-        title: "Sie habe sich erfolgreich registriert!",
-        text: "Sie können sich jetzt mit ihren Zugangsdaten einloggen.",
-        effect: 'slide',
-        speed: 300,
-        status: 'success',
-        autoclose: true,
-        autotimeout: 5000,
-        position: 'right bottom',
-        gap: 20,
-        distance: 70
-    })
-}
