b2559de30293b9cd3aa6059711768d84f9366824
Sebastian Eisele <b4stygp@googlemail.com>
2021-05-31 14:12:23 +0200

    Kommentare hinzugefügt

--- js/functionScripts.js
+++ js/functionScripts.js
@@ -1,4 +1,4 @@
-function einAusblendenLoginRegForm()
+function einAusblendenLoginRegForm() //Funktion zum einblenden des Register Formulars.
 {
     var loginForm = document.getElementById("LandingLogin");
     var loginFormStyle = getComputedStyle(loginForm);
@@ -16,30 +16,30 @@ function einAusblendenLoginRegForm()
     console.log("regForm: " + registerFormStyle);
 
 
-    if(logBool && regBool)
+    if(logBool && regBool)  //beides visible -> beides hidden
     {
         console.log("L:visible R:visible");
         document.getElementById("LandingLogin").style.visibility="hidden";
         document.getElementById("RegisterForm").style.visibility="hidden";
     }
-    else if(logBool && !regBool)
+    else if(logBool && !regBool) //log visible reg hidden -> log hidden
     {
         console.log("L:visible R:hidden");
         document.getElementById("LandingLogin").style.visibility="hidden";
     }
-    else if(!logBool && regBool)
+    else if(!logBool && regBool) // log hidden reg visible -> ref hidden
     {
         console.log("L:hidden R:visible");
         document.getElementById("RegisterForm").style.visibility="hidden";
     }
-    else if(!logBool && !regBool)
+    else if(!logBool && !regBool) // beides hidden -> log visible
     {
         console.log("L:hidden R:hidden");
         document.getElementById("LandingLogin").style.visibility="visible"
     }
 }
 
-function nichtRegistriertHandler(){
+function nichtRegistriertHandler(){ //Wenn im Login-Form auf "noch nicht regstriert geklickt wird
     einAusblendenLoginRegForm();
 
     var registerForm = document.getElementById("RegisterForm");
@@ -47,16 +47,8 @@ function nichtRegistriertHandler(){
     var hidden = "hidden";
     var regBool = registerFormStyle.visibility.localeCompare(hidden);
 
-    if(!regBool)
+    if(!regBool) //reg hidden -> visible
     {
         document.getElementById("RegisterForm").style.visibility="visible";
     }
-}
-
-function hoverAGBs(farbe){
-	document.getElementById('AGB').style.color=farbe;
-}
-
-function clickAGBs(){
-    window.open();
 }
