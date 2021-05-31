12984d311c052f162e379eb7a1eb84bfbd75909d
tobitoast4 <tobi-30@gmx.de>
2021-05-31 11:32:58 +0200

    Update bestellungen.php

--- content/bestellungen.php
+++ content/bestellungen.php
@@ -178,8 +178,8 @@
                             account_icon = account_icon + '<i class="fas fa-user-circle" style="color: #ffffff"></i>';
                             account_icon = account_icon + '</a>';
                             account_icon = account_icon + '<ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">';
-                            account_icon = account_icon + '<li><a class="dropdown-item" href="./content/profil.php">Profil</a></li>';
-                            account_icon = account_icon + '<li><a class="dropdown-item" href="./content/bestellungen.php">Bestellungen</a></li>';
+                            account_icon = account_icon + '<li><a class="dropdown-item" href="./profil.php">Profil</a></li>';
+                            account_icon = account_icon + '<li><a class="dropdown-item" href="./bestellungen.php">Bestellungen</a></li>';
                             account_icon = account_icon + '<li><hr class="dropdown-divider" /></li>';
                             account_icon = account_icon + '<li><a class="dropdown-item" href="#" onclick="logout()">Logout</a></li>';
                             account_icon = account_icon + '</ul>';
